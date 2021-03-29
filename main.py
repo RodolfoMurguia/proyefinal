c9508c9139335de57dbdb2158620958a726e274c
“Javier <frjavaro87@gmail.com>
2021-03-28 19:33:12 -0600

    emaseo

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 """programa principal"""
 
+print("Bienvenido al programa de nóminas")
+print("Opciones de búsqueda")
+print("a: Puestos")
+print("b: Salarios")
+print("c: Empleados")
+
+option = input("Qué quieres consultar?: ")
+
